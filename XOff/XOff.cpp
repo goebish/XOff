c1bff37a1caf7b25982eaacb8fbfb3acef7a456f
Goebish <goebish@gmail.com>
2015-12-21 11:00:55 +0100

    Edit license text

--- XOff/XOff.cpp
+++ XOff/XOff.cpp
@@ -7,13 +7,13 @@
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- Deviation is distributed in the hope that it will be useful,
+ This project is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ along with this project.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
